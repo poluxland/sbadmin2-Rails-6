5283912982199878a8956c97b6defaeb9cd3f2d9
Jose Jerez <joseignaciojerez@gmail.com>
2020-09-02 12:09:45 -0400

    ruby 2.7 y modificacion lenguaje

--- app/controllers/examples_controller.rb
+++ app/controllers/examples_controller.rb
@@ -6,16 +6,16 @@ class ExamplesController < ApplicationController
 
   def set_variables
     @notifications = [
-      OpenStruct.new(date: 'December 12, 2019', content: 'A new monthly report is ready to download!', kind: 'primary', viewed: false),
-      OpenStruct.new(date: 'December 7, 2019', content: '$290.29 has been deposited into your account!', kind: 'success', viewed: true),
-      OpenStruct.new(date: 'December 2, 2019', content: 'Spending Alert: We\'ve noticed unusually high spending for your account.', kind: 'warning', viewed: true)
+      OpenStruct.new(date: 'December 12, 2020', content: 'Nuevo reporte de confiabilidad disponbile para descargar', kind: 'primary', viewed: false),
+      OpenStruct.new(date: 'December 7, 2020', content: 'Proyecto NGP finalizado', kind: 'success', viewed: true),
+      OpenStruct.new(date: 'December 2, 2020', content: 'Alerta proyecto BRU fuera de plazo', kind: 'warning', viewed: true)
     ]
 
     @messages = [
-      OpenStruct.new(content: "Hi there! I am wondering if you can help me with a problem I've been having.", sender: "Emily Fowler · 58m", sender_avatar: 'https://source.unsplash.com/fn_BT9fwg_E/60x60'),
-      OpenStruct.new(content: "I have the photos that you ordered last month, how would you like them sent to you?", sender: "Jae Chun · 1d", sender_avatar: 'https://source.unsplash.com/AU4VPcFN4LE/60x60'),
-      OpenStruct.new(content: "Last month's report looks great, I am very happy with the progress so far, keep up the good work!", sender: "Morgan Alvarez · 2d", sender_avatar: 'https://source.unsplash.com/CS2uCrpNzJY/60x60'),
-      OpenStruct.new(content: "Am I a good boy? The reason I ask is because someone told me that people say this to all dogs, even if they aren't good...", sender: "Chicken the Dog · 2w", sender_avatar: 'https://source.unsplash.com/Mv9hjnEUHR4/60x60')
+      OpenStruct.new(content: "Consulta por problema en BRU", sender: "Emily Fowler · 58m", sender_avatar: 'https://source.unsplash.com/fn_BT9fwg_E/60x60'),
+      OpenStruct.new(content: "Fotografias M21 disponibles para entrega", sender: "Jae Chun · 1d", sender_avatar: 'https://source.unsplash.com/AU4VPcFN4LE/60x60'),
+      OpenStruct.new(content: "Gran reporte del ultimo mes", sender: "Morgan Alvarez · 2d", sender_avatar: 'https://source.unsplash.com/CS2uCrpNzJY/60x60'),
+      OpenStruct.new(content: "Reunión paro de horno aplazada", sender: "Chicken the Dog · 2w", sender_avatar: 'https://source.unsplash.com/Mv9hjnEUHR4/60x60')
     ]
   end
 end
